00ae67e1a2c41c0724136bc88e3a6ee4597dfde0
Omar Ashraf <Omarlabib.51237@gmail.com>
2024-05-04 13:02:14 +0300

    apply demo to illustrate the builder

--- Builder/src/Main.java
+++ Builder/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Car car = new CarBuilder()
+            .Color("Red")
+            .Brand("Toyota")
+            .Year(2020)
+            .Price(20000)
+            .build();
+        System.out.println("Color: " + car.getColor());
+        System.out.println("Brand: " + car.getBrand());
+        System.out.println("Year: " + car.getYear());
+        System.out.println("Price: " + car.getPrice());
     }
 }
